9cf38bd812086de76024ff9fb8932b1b6fa920c9
Bryan Redd <bryan.d.redd@gmail.com>
2023-12-22 15:37:14 -0700

    2015 day 17

--- 2015/day17/day17_test.go
+++ 2015/day17/day17_test.go
@@ -1,1 +1,23 @@
 package main
+
+import "testing"
+
+func TestDay17(t *testing.T) {
+	containers := []int{20, 15, 10, 5, 5}
+	target := 25
+
+	if have, want := Start(containers, target), 4; have != want {
+		t.Errorf("have %d, want %d", have, want)
+	}
+
+	if have, want := Start2(containers, target), 2; have != want {
+		t.Errorf("have %d, want %d", have, want)
+	}
+
+	containers = ParseInput(`input\input.txt`)
+	target = 150
+
+	if have, want := Start(containers, target), 4372; have != want {
+		t.Errorf("have %d, want %d", have, want)
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Advent of Code 
 
 This contains my solutions for the Advent of Code events.
+
+## Progress
+
+|Year|⭐|
+|---|---|
+|2015|34|
+|2023|44|
