47fe64e4a926e203b4a1b6623f921e28a55e83e7
Bryan Redd <bryan.d.redd@gmail.com>
2024-06-06 20:31:05 -0600

    2016 day 01

--- 2016/day01/day01.go
+++ 2016/day01/day01.go
@@ -1,5 +1,154 @@
 package main
 
-func main() {
+import (
+	_ "embed"
+	"fmt"
+	"math"
+	"strings"
+)
 
+const (
+	dir_N = iota
+	dir_E
+	dir_W
+	dir_S
+)
+
+func Distance1(input string) int {
+
+	instructions := strings.Split(input, ", ")
+
+	x := 0
+	y := 0
+	dir := dir_N
+
+	for _, i := range instructions {
+		var turn byte = i[0]
+		var dist int
+		fmt.Sscanf(i[1:], "%d", &dist)
+
+		switch turn {
+		case 'L':
+			switch dir {
+			case dir_N:
+				dir = dir_W
+			case dir_E:
+				dir = dir_N
+			case dir_W:
+				dir = dir_S
+			case dir_S:
+				dir = dir_E
+			}
+		case 'R':
+			switch dir {
+			case dir_N:
+				dir = dir_E
+			case dir_E:
+				dir = dir_S
+			case dir_W:
+				dir = dir_N
+			case dir_S:
+				dir = dir_W
+			}
+		}
+
+		switch dir {
+		case dir_N:
+			y += dist
+		case dir_E:
+			x += dist
+		case dir_W:
+			x -= dist
+		case dir_S:
+			y -= dist
+		}
+	}
+
+	return int(math.Abs(float64(x)) + math.Abs(float64(y)))
+}
+
+func checkVisits(visits *map[int]map[int]interface{}, x, y int) bool {
+	if _, ok := (*visits)[x]; !ok {
+		(*visits)[x] = make(map[int]interface{})
+	}
+
+	if _, ok := (*visits)[x][y]; !ok {
+		(*visits)[x][y] = nil
+		return false
+	}
+
+	return true
+}
+
+func Distance2(input string) int {
+
+	instructions := strings.Split(input, ", ")
+
+	x := 0
+	y := 0
+	dir := dir_N
+
+	visits := make(map[int](map[int]interface{}))
+	visits[0] = make(map[int]interface{})
+	visits[0][0] = nil
+
+outer:
+	for _, i := range instructions {
+		var turn byte = i[0]
+		var dist int
+		fmt.Sscanf(i[1:], "%d", &dist)
+
+		switch turn {
+		case 'L':
+			switch dir {
+			case dir_N:
+				dir = dir_W
+			case dir_E:
+				dir = dir_N
+			case dir_W:
+				dir = dir_S
+			case dir_S:
+				dir = dir_E
+			}
+		case 'R':
+			switch dir {
+			case dir_N:
+				dir = dir_E
+			case dir_E:
+				dir = dir_S
+			case dir_W:
+				dir = dir_N
+			case dir_S:
+				dir = dir_W
+			}
+		}
+
+		for i := 0; i < dist; i++ {
+			switch dir {
+			case dir_N:
+				y += 1
+			case dir_E:
+				x += 1
+			case dir_W:
+				x -= 1
+			case dir_S:
+				y -= 1
+			}
+
+			visited := checkVisits(&visits, x, y)
+			if visited {
+				break outer
+			}
+		}
+	}
+
+	return int(math.Abs(float64(x)) + math.Abs(float64(y)))
+}
+
+//go:embed input/input.txt
+var input string
+
+func main() {
+	fmt.Printf("Part 1: %d\n", Distance1(input))
+	fmt.Printf("Part 2: %d\n", Distance2(input))
 }
--- 2016/day01/day01_test.go
+++ 2016/day01/day01_test.go
@@ -1,1 +1,27 @@
 package main
+
+import "testing"
+
+func TestDistance1(t *testing.T) {
+
+	if have, want := Distance1("R2, L3"), 5; have != want {
+		t.Errorf("have %d, want %d", have, want)
+	}
+
+	if have, want := Distance1("R2, R2, R2"), 2; have != want {
+		t.Errorf("have %d, want %d", have, want)
+	}
+
+	if have, want := Distance1("R5, L5, R5, R3"), 12; have != want {
+		t.Errorf("have %d, want %d", have, want)
+	}
+
+}
+
+func TestDistance2(t *testing.T) {
+
+	if have, want := Distance2("R8, R4, R4, R8"), 4; have != want {
+		t.Errorf("have %d, want %d", have, want)
+	}
+
+}
