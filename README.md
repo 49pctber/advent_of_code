55136ef03cce8c408be19921e8d18860c8fa82ee
Bryan Redd <bryan.d.redd@gmail.com>
2024-06-07 17:52:10 -0600

    Cleaned up repo to only include completed days

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Advent of Code
 
 This contains my solutions for the Advent of Code events.
+
+## `cmd\template.go`
+
+Running this will create a template for a new day.
---
+++ cmd/template/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func Part1() {
+	fmt.Printf("Part 1: %v\n", "x")
+}
+
+func Part2() {
+	fmt.Printf("Part 2: %v\n", "x")
+}
+
+func main() {
+	Part1()
+	Part2()
+}
