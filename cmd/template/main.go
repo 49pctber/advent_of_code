b3a2a11da3d83b6cfe24a9affebde968e4b33b5f
Bryan Redd <bryan.d.redd@gmail.com>
2024-06-10 16:59:42 -0600

    Updated templates

--- cmd/template/main.go
+++ cmd/template/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func Part1() {
-	fmt.Printf("Part 1: %v\n", "x")
+	fmt.Printf("Part 1: %v\n", "?")
 }
 
 func Part2() {
-	fmt.Printf("Part 2: %v\n", "x")
+	fmt.Printf("Part 2: %v\n", "?")
 }
 
 func main() {
