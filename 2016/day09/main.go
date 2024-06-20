fb5a587eb5cc3ad2900d21f8d431507a705238b3
Bryan Redd <bryan.d.redd@gmail.com>
2024-06-20 16:33:50 -0600

    2016 day 09

--- 2016/day09/main.go
+++ 2016/day09/main.go
@@ -36,6 +36,32 @@ func DecompressedLength(s string) int {
 	}
 }
 
+// I don't think this case works in general,
+// but it *does* work for my input ¯\_(ツ)_/¯
+func DecompressedLength2(s string) int {
+	l := 0
+	r := regexp.MustCompile(`^(\w*)\((\d+)x(\d+)\)`)
+	for {
+		loc := r.FindStringSubmatch(s)
+		if loc == nil {
+			return l + len(s)
+		} else {
+			x := len(loc[0])
+			l += len(loc[1])
+			a, err := strconv.Atoi(loc[2])
+			if err != nil {
+				panic(err)
+			}
+			b, err := strconv.Atoi(loc[3])
+			if err != nil {
+				panic(err)
+			}
+			l += b * DecompressedLength2(s[x:x+a])
+			s = s[x+a:]
+		}
+	}
+}
+
 func main() {
 	b, err := os.ReadFile(os.Args[1])
 	if err != nil {
@@ -43,5 +69,5 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %v\n", DecompressedLength(string(b)))
-	fmt.Printf("Part 1: %v\n", DecompressedLength2(string(b)))
+	fmt.Printf("Part 2: %v\n", DecompressedLength2(string(b)))
 }
--- 2016/day09/main_test.go
+++ 2016/day09/main_test.go
@@ -29,7 +29,19 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
-	if have, want := 0, 0; have != want {
+	if have, want := DecompressedLength2(`(3x3)XYZ`), 9; have != want {
+		t.Errorf("have %v, want %v", have, want)
+	}
+
+	if have, want := DecompressedLength2(`X(8x2)(3x3)ABCY`), 20; have != want {
+		t.Errorf("have %v, want %v", have, want)
+	}
+
+	if have, want := DecompressedLength2(`(27x12)(20x12)(13x14)(7x10)(1x12)A`), 241920; have != want {
+		t.Errorf("have %v, want %v", have, want)
+	}
+
+	if have, want := DecompressedLength2(`(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN`), 445; have != want {
 		t.Errorf("have %v, want %v", have, want)
 	}
 }
