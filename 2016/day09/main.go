46895ef29f67e032dd1209d04433f9417a55dd22
Bryan Redd <bryan.d.redd@gmail.com>
2024-06-20 14:52:08 -0600

    temp

--- 2016/day09/main.go
+++ 2016/day09/main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 
 	_ "embed"
 )
 
-func DecompressedLength(input string) int {
-	return 0
-}
+func DecompressedLength(s string) int {
+	l := 0
+	r := regexp.MustCompile(`\((\d+)x(\d+)\)`)
 
-func Part1() {
-	fmt.Printf("Part 1: %v\n", "?")
-}
+	for {
+		loc := r.FindStringSubmatch(s)
+		if loc == nil {
+			return l + len(s)
+		} else {
 
-func Part2() {
-	fmt.Printf("Part 2: %v\n", "?")
+			a, err := strconv.Atoi(loc[1])
+			if err != nil {
+				panic(err)
+			}
+
+			b, err := strconv.Atoi(loc[2])
+			if err != nil {
+				panic(err)
+			}
+
+			s = s[len(loc[0])+a:]
+
+			l += a * b
+		}
+	}
 }
 
 func main() {
-	Part1()
-	Part2()
+	b, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Part 1: %v\n", DecompressedLength(string(b)))
+	fmt.Printf("Part 1: %v\n", DecompressedLength2(string(b)))
 }
