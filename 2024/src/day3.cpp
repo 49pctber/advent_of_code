0d7eaec730d777ff94a4592a6c64cfb0c37d7e9d
Bryan Redd <bryan.d.redd@gmail.com>
2024-12-03 16:19:13 -0700

    much more elegant solution

--- 2024/src/day3.cpp
+++ 2024/src/day3.cpp
@@ -36,59 +36,28 @@ void Solution::part2() {
 
     std::string line;
     int sum = 0;
-    std::regex pattern(R"(mul\(([0-9]{1,3}),([0-9]{1,3})\))");
-    std::regex pattern_do(R"(do\(\))");
-    std::regex pattern_dont(R"(don't\(\))");
+    std::regex pattern(R"(mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\))");
+    bool ignore = false;
 
-    std::vector<int> donts;
-    std::vector<int> dos;
-    int offset = 0;
     while (std::getline(file, line)) {
-        // std::cout << line << std::endl;
-        auto begin_dont =
-            std::sregex_iterator(line.begin(), line.end(), pattern_dont);
-        auto end_dont = std::sregex_iterator();
-
-        for (auto it = begin_dont; it != end_dont; ++it) {
-            std::smatch match = *it;
-            std::cout << match.position() << " ";
-            donts.push_back(match.position() + offset);
-        }
-
-        auto begin_do =
-            std::sregex_iterator(line.begin(), line.end(), pattern_do);
-        auto end_do = std::sregex_iterator();
-
-        for (auto it = begin_do; it != end_do; ++it) {
-            std::smatch match = *it;
-            // std::cout << match.position() << " ";
-            dos.push_back(match.position() + offset);
-        }
 
         auto begin = std::sregex_iterator(line.begin(), line.end(), pattern);
         auto end = std::sregex_iterator();
         for (auto it = begin; it != end; ++it) {
             std::smatch match = *it;
-            int pos = match.position() + offset;
-            int dontpos = -2;
-            int dopos = -1;
 
-            for (int n : donts) {
-                if (n < pos) {
-                    dontpos = n;
+            if (match.str() == "do()") {
+                ignore = false;
+            } else if (match.str() == "don't()") {
+                ignore = true;
+            } else {
+                if (!ignore) {
+                    sum +=
+                        std::stoi(match[1], NULL) * std::stoi(match[2], NULL);
                 }
-            }
-            for (int n : dos) {
-                if (n < pos) {
-                    dopos = n;
-                }
-            }
-            if (dopos > dontpos) {
-                sum += std::stoi(match[1], NULL) * std::stoi(match[2], NULL);
             }
         }
-        offset += line.size();
     }
 
-    std::cout << "Part 2: " << sum << std::endl; // not 66677244
+    std::cout << "Part 2: " << sum << std::endl;
 }
