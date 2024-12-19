3f22d9fd03a0b095338da111a43611b8fa464d05
Bryan Redd <bryan.d.redd@gmail.com>
2024-12-19 08:43:17 -0700

    2024 day 19 part 1

--- cpp/src/2024/day19.cpp
+++ cpp/src/2024/day19.cpp
@@ -1,7 +1,9 @@
 #include "solution.hpp"
 #include <fstream>
 #include <iostream>
+#include <regex>
 #include <string>
+#include <vector>
 
 void Solution::part1() {
     std::ifstream file(argv[1]);
@@ -11,8 +13,33 @@ void Solution::part1() {
     }
 
     std::string line;
-    while (std::getline(file, line)) {
+    std::getline(file, line);
+
+    std::string prefix = "^((";
+    std::string postfix = "))+$";
+    std::string from = ", ";
+    std::string to = ")|(";
+
+    size_t pos = 0;
+    while ((pos = line.find(from, pos)) != std::string::npos) {
+        line.replace(pos, from.length(), to);
+        pos += to.length();
     }
+
+    std::regex pattern(prefix + line + postfix);
+
+    int n_possible = 0;
+    std::getline(file, line); // ignore blank line
+    while (std::getline(file, line)) {
+        // std::cout << line << ": ";
+        if (std::regex_match(line, pattern)) {
+            // std::cout << "match!\n";
+            n_possible++;
+        } else {
+            // std::cout << "no match.\n";
+        }
+    }
+    std::cout << "Part 1: " << n_possible << std::endl;
 }
 
 void Solution::part2() {}
