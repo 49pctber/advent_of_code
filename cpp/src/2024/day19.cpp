4420297fefc32390e0d6e661b7d1d7695b79479d
Bryan Redd <bryan.d.redd@gmail.com>
2024-12-19 09:32:17 -0700

    2024 day 19

--- cpp/src/2024/day19.cpp
+++ cpp/src/2024/day19.cpp
@@ -1,7 +1,9 @@
 #include "solution.hpp"
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <regex>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -31,15 +33,65 @@ void Solution::part1() {
     int n_possible = 0;
     std::getline(file, line); // ignore blank line
     while (std::getline(file, line)) {
-        // std::cout << line << ": ";
         if (std::regex_match(line, pattern)) {
-            // std::cout << "match!\n";
             n_possible++;
-        } else {
-            // std::cout << "no match.\n";
         }
     }
     std::cout << "Part 1: " << n_possible << std::endl;
 }
 
-void Solution::part2() {}
+long int count(std::set<std::string> &tokens, std::string s,
+               std::map<std::string, long int> &memos) {
+
+    // check if end of string
+    if (s.length() == 0) {
+        return 1;
+    }
+
+    // check for memo
+    if (memos.find(s) != memos.end()) {
+        return memos[s];
+    }
+
+    // remove any prefixes and recursively count number of possibilities
+    long int c = 0;
+    for (auto token : tokens) {
+        if (s.substr(0, token.size()) == token) {
+            c += count(tokens, s.substr(token.size(), s.size()), memos);
+        }
+    }
+
+    // add memo
+    memos[s] = c;
+
+    return c;
+}
+
+void Solution::part2() {
+    std::ifstream file(argv[1]);
+    if (!file.is_open()) {
+        std::cerr << "error opening file\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::map<std::string, long int> memos;
+    std::set<std::string> tokens;
+    std::string line;
+    std::getline(file, line);
+
+    std::stringstream ss(line);
+    std::string token;
+    while (std::getline(ss, token, ',')) {
+        if (token[0] == ' ') {
+            token = token.substr(1, token.size());
+        }
+        tokens.insert(token);
+    }
+
+    long int c = 0;
+    std::getline(file, line); // ignore blank line
+    while (std::getline(file, line)) {
+        c += count(tokens, line, memos);
+    }
+    std::cout << "Part 2: " << c << std::endl;
+}
