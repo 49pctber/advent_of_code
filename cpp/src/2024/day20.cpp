d004153b493695da8febc90c62b779d1e3651bf0
Bryan Redd <bryan.d.redd@gmail.com>
2024-12-20 07:30:46 -0700

    2024 day 20

--- cpp/src/2024/day20.cpp
+++ cpp/src/2024/day20.cpp
@@ -99,4 +99,92 @@ void Solution::part1() {
     std::cout << "Part 1: " << count << std::endl;
 }
 
-void Solution::part2() {}
+void Solution::part2() {
+    std::ifstream file(argv[1]);
+    if (!file.is_open()) {
+        std::cerr << "error opening file\n";
+        exit(EXIT_FAILURE);
+    }
+
+    std::string line;
+    grid_t grid;
+    orientation_t start;
+    while (std::getline(file, line)) {
+        std::vector<cell_t> row(line.size());
+        for (int i = 0; i < line.size(); i++) {
+            row[i].position.row = grid.size();
+            row[i].position.col = i;
+            row[i].type = line[i];
+            if (line[i] == 'S') {
+                start.position.row = grid.size();
+                start.position.col = i;
+                start.direction = dir_right;
+                if (line[i - 1] == '.') {
+                    start.direction = dir_left;
+                }
+            }
+        }
+        grid.push_back(row);
+    }
+
+    fill_direction(grid, start, 0);
+
+    long int count = 0;
+    std::map<int, int> cheat_counts;
+
+    const int max_dur = 20;
+    const int min_cheat = 100;
+
+    for (auto row : grid) {
+        for (auto cell : row) {
+            if (cell.type == '#') {
+                continue;
+            }
+
+            int dist1 = cell.distance;
+
+            for (int drow = 0; drow <= max_dur; drow++) {
+
+                if (cell.position.row + drow >= grid.size() - 1) {
+                    continue;
+                }
+
+                for (int dcol = drow > 0 ? -(max_dur - drow) : 1;
+                     dcol <= max_dur - drow; dcol++) {
+
+                    if (cell.position.col + dcol < 1) {
+                        continue;
+                    } else if (cell.position.col + dcol >= row.size() - 1) {
+                        break;
+                    }
+
+                    cell_t *other = &(grid[cell.position.row + drow]
+                                          [cell.position.col + dcol]);
+
+                    if (other->type == '#') {
+                        continue;
+                    }
+                    int dist2 = other->distance;
+
+                    int manhattan = std::abs(drow) + std::abs(dcol);
+
+                    int cheat = std::abs(dist2 - dist1) - manhattan;
+
+                    if (cheat > 0) {
+                        cheat_counts[cheat]++;
+                    }
+
+                    if (cheat >= min_cheat) {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    // for (auto x : cheat_counts) {
+    //     std::cout << x.first << ": " << x.second << '\n';
+    // }
+
+    std::cout << "Part 2: " << count << std::endl;
+}
