b2b312fbd3c8e14438e4532d9d9203bb30f5c6f3
Bryan Redd <bryan.d.redd@gmail.com>
2024-12-25 04:37:05 -0700

    MERRY CHRISTMAS!

--- cpp/src/2024/day25.cpp
+++ cpp/src/2024/day25.cpp
@@ -1,6 +1,8 @@
 #include "solution.hpp"
+#include <array>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <string>
 
 void Solution::part1() {
@@ -10,9 +12,51 @@ void Solution::part1() {
         exit(EXIT_FAILURE);
     }
 
+    std::set<std::array<int, 5>> locks;
+    std::set<std::array<int, 5>> keys;
+
     std::string line;
     while (std::getline(file, line)) {
+        bool lock = line == "#####";
+
+        std::array<int, 5> pins{0, 0, 0, 0, 0};
+
+        for (int i = 0; i < 5; i++) {
+            std::getline(file, line);
+            for (int c = 0; c < 5; c++) {
+                if (line[c] == '#') {
+                    pins[c]++;
+                }
+            }
+        }
+        std::getline(file, line);
+        std::getline(file, line);
+
+        if (lock) {
+            locks.insert(pins);
+        } else {
+            keys.insert(pins);
+        }
     }
+
+    // std::cout << "Locks: " << locks.size() << "\n";
+    // std::cout << "Keys:  " << keys.size() << "\n";
+
+    int count = 0;
+    for (auto lock : locks) {
+        for (auto key : keys) {
+            bool fits = true;
+            for (int i = 0; i < 5; i++) {
+                if (lock[i] + key[i] > 5) {
+                    fits = false;
+                }
+            }
+            if (fits) {
+                count++;
+            }
+        }
+    }
+    std::cout << "Part 1: " << count << std::endl;
 }
 
 void Solution::part2() {}
